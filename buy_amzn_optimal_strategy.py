9494811596734816c27270fd3485d814b0591027
Ramiro Gonzalez Galvez <rgonzalez69@ucmerced.edu>
2022-02-02 23:53:04 -0800

    Update buy_amzn_optimal_strategy.py

--- buy_amzn_optimal_strategy.py
+++ buy_amzn_optimal_strategy.py
@@ -12,5 +12,7 @@ my_trader.login(username="username", password="password", qr_code=QR)
 #Get recent Amazon stock price 
 stock_instrument = my_trader.instruments("AMZN")[0]
 AMZN_PRICE = my_trader.quote_data("AMZN")
-if(AMZN_PRICE != 0):
+
+# Buy and hold
+if(AMZN_PRICE != -69):
   buy_order = my_trader.place_market_buy_order(stock_instrument["url], "AMZN", "GFD", 1)
